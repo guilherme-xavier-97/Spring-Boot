e8bf15430dac03d00b06119984149cc2c805f3bb
Guilherme Xavier <guilhermexavier2007@hotmail.com>
2022-03-07 16:46:50 -0300

    Criação do BD H2

--- src/main/java/com/guilherme/cursospring/domain/Categoria.java
+++ src/main/java/com/guilherme/cursospring/domain/Categoria.java
@@ -1,16 +1,25 @@
 package com.guilherme.cursospring.domain;
 
-import java.io.Serializable;
-import java.util.Objects;
 
+import java.io.Serializable;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
 public class Categoria implements Serializable {
-	
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
 	public Categoria() {
-		
 	}
 
 	public Categoria(Integer id, String nome) {
@@ -34,16 +43,20 @@ public class Categoria implements Serializable {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
+
+
 	/* O hash cria "atalhos para facilitar a busca. EX: vc vai buscar o nome Guilherme no BD
 	 * o hash faz a busca ir em ordem alfabetica, por exemplo, assim, vai ignorar todas as letras
 	 * e ir direto pro G, tornando a busca mais rápida.
 	 */
-	  
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
+
 	
 	// o equals compara o valor exato de cada objeto
 	@Override
@@ -55,8 +68,17 @@ public class Categoria implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Categoria other = (Categoria) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
-	
-	
+
 }
+	
+	  
+	
+	
+
