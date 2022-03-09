565d7cedd0eac2ace9858615e5f05a22df13c55b
Guilherme Xavier <guilhermexavier2007@hotmail.com>
2022-03-09 11:08:53 -0300

    Criação método POST para Categorias

--- src/main/java/com/guilherme/cursospring/resources/CategoriaResource.java
+++ src/main/java/com/guilherme/cursospring/resources/CategoriaResource.java
@@ -1,12 +1,16 @@
 package com.guilherme.cursospring.resources;
 
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.guilherme.cursospring.domain.Categoria;
 import com.guilherme.cursospring.services.CategoriaService;
@@ -21,9 +25,19 @@ public class CategoriaResource {
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		
-		Categoria obj = service.buscar(id);
-		return ResponseEntity.ok().body(obj);
+		Categoria obj = service.read(id);
+		return ResponseEntity.ok().body(obj); 
 		
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) { //O Request Body converte os dados JSON para um objeto JAVA
+		obj = service.insert(obj);
+		
+		/*Esse método serve pra quando vc adicionar uma categoria nova, 
+		automaticamente já gera a URI de acesso a ela. Fica na aba "Heade" do Insomnia */
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 }
--- src/main/java/com/guilherme/cursospring/services/CategoriaService.java
+++ src/main/java/com/guilherme/cursospring/services/CategoriaService.java
@@ -15,9 +15,14 @@ public class CategoriaService {
 	@Autowired //serve pra instanciar objetos por meio da injeção de dependecia
 	private CategoriaRepository repo;
 	
-	public Categoria buscar(Integer id) {
+	public Categoria read(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + 
 		Categoria.class.getName()));
+	}
+	
+	public Categoria insert (Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
 	}
 }
