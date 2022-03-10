4900cd741472cf1cdb14e5bb82ca641c9daf28cb
Guilherme Xavier <guilhermexavier2007@hotmail.com>
2022-03-10 08:45:31 -0300

    POST para Clientes

--- src/main/java/com/guilherme/cursospring/resources/ClientesResource.java
+++ src/main/java/com/guilherme/cursospring/resources/ClientesResource.java
@@ -1,5 +1,6 @@
 package com.guilherme.cursospring.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,9 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.guilherme.cursospring.domain.Cliente;
 import com.guilherme.cursospring.dto.ClienteDTO;
+import com.guilherme.cursospring.dto.ClienteTelefoneEnderecoDTO;
 import com.guilherme.cursospring.services.ClienteService;
 
 @RestController
@@ -61,6 +64,16 @@ public class ClientesResource {
 		
 		return ResponseEntity.ok().body(listDTO); 
 		
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteTelefoneEnderecoDTO objDTO) { //O Request Body converte os dados JSON para um objeto JAVA
+		Cliente obj = service.fromDTO(objDTO);
+		obj = service.insert(obj);
+		/*Esse método serve pra quando vc adicionar uma categoria nova, 
+		automaticamente já gera a URI de acesso a ela. Fica na aba "Heade" do Insomnia */
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
--- src/main/java/com/guilherme/cursospring/services/ClienteService.java
+++ src/main/java/com/guilherme/cursospring/services/ClienteService.java
@@ -9,10 +9,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.guilherme.cursospring.domain.Cidades;
 import com.guilherme.cursospring.domain.Cliente;
+import com.guilherme.cursospring.domain.Endereco;
+import com.guilherme.cursospring.domain.enums.TipoCliente;
 import com.guilherme.cursospring.dto.ClienteDTO;
+import com.guilherme.cursospring.dto.ClienteTelefoneEnderecoDTO;
 import com.guilherme.cursospring.repositories.ClienteRepository;
+import com.guilherme.cursospring.repositories.EnderecoRepository;
 import com.guilherme.cursospring.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -20,6 +26,9 @@ public class ClienteService {
 	
 	@Autowired //serve pra instanciar objetos por meio da injeção de dependecia
 	private ClienteRepository repo;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 	
 	public Cliente readOne(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
@@ -29,6 +38,14 @@ public class ClienteService {
 
 	public List<Cliente> readAll() {
 		return repo.findAll();
+	}
+	
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
 	}
 
 	
@@ -59,6 +76,27 @@ public class ClienteService {
 	//Essa função converte um objeto tipo Cliente para tipo Cliente DTO
 	public Cliente fromDTO(ClienteDTO objDTO) {
 		return new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail(), null, null);	
+	}
+	
+	public Cliente fromDTO(ClienteTelefoneEnderecoDTO objDTO) {
+		Cliente cli = new Cliente(null, objDTO.getNome(), objDTO.getEmail(), objDTO.getCpfOucnpj(), 
+				TipoCliente.toEnum(objDTO.getTipo()));	
+		
+		Cidades cid = new Cidades(objDTO.getCidadeId(), null, null);
+		
+		Endereco end = new Endereco(null, objDTO.getLogradouro(), objDTO.getNumero(), objDTO.getComplemento(), 
+				objDTO.getBairro(), objDTO.getCep(), cli, cid);
+		
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDTO.getTelefone1());
+		if(objDTO.getTelefone2() != null) {
+			cli.getTelefones().add(objDTO.getTelefone2());
+		}
+		if(objDTO.getTelefone3() != null) {
+			cli.getTelefones().add(objDTO.getTelefone3());
+		}
+		
+		return cli;
 	}
 	
 	/*
