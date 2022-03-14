b97ab00c7bfbebfeb7e0a5809b3c66b9ed8a089e
Guilherme Xavier <guilhermexavier2007@hotmail.com>
2022-03-14 13:46:00 -0300

    Inserindo dados no BD do heroku (testando)

---
+++ src/main/java/com/guilherme/cursospring/config/ProdConfig.java
@@ -0,0 +1,36 @@
+package com.guilherme.cursospring.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.guilherme.cursospring.services.DBService;
+
+@Configuration
+@Profile("prod")
+public class ProdConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	//Aqui eu pego o valor la do arquivo application-dev.properties e salvo na variavel strategy
+	@Value("{spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		
+		//Aqui eu testo se o valor da variavel strategy recebeu um create ou não. Se não for create, n faz nada, se for, cria as tabelas no DB
+		if(!"create".equals(strategy)) {
+			return false;
+		}
+		
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+
+}
