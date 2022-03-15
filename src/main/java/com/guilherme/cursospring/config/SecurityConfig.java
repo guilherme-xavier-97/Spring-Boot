5e0e9f3caa14dd91fbbdb952d8bce04e71b30467
Guilherme Xavier <guilhermexavier2007@hotmail.com>
2022-03-15 11:35:55 -0300

    Configuração inicial do Spring Security

---
+++ src/main/java/com/guilherme/cursospring/config/SecurityConfig.java
@@ -0,0 +1,64 @@
+package com.guilherme.cursospring.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private Environment env;
+	
+	//Esse vetor indica quais endpoint eu vou querer liberar acesso
+	private static final String[] PUBLIC_MATCHERS = {
+			"/h2-console/**",
+
+	};
+	
+	//Esse vetor indica qual tipo de acesso vou liberar no endpoint, nesse caso só requisições do tipo GET são aceitas
+	private static final String[] PUBLIC_MATCHERS_GET = {
+			"/produtos/**",
+			"/categorias/**"
+	};
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		//Se eu não fizer isso,não consigo acessar a URL do H2 no navegador
+		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+            http.headers().frameOptions().disable();
+        }
+		
+		//Eu desabilito a proteção contra CSRF que sao ataques feitos usando a seesão ativa, como o app é stateless, não guarda sessão, então não precisa dessa proteção
+		http.cors().and().csrf().disable();
+		//Esse método informa os endpoints liberados e diz que fora eles, todos os outros precisam de autenticação
+		http.authorizeRequests()
+			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+			.antMatchers(PUBLIC_MATCHERS).permitAll()
+			.anyRequest().authenticated();
+		
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+	
+	//Como o app vai ser acessado por multiplas fontes (ambiente de teste, de desenvolvimento e produção) eu preciso liberar os acessos
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		return source;
+	}
+}
+
+
