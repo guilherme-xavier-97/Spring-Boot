46a8747ba7777f1507d4330df66b23156ca346a3
Guilherme Xavier <guilhermexavier2007@hotmail.com>
2022-03-21 10:12:47 -0300

    CORREÇÃO DOS BUGS, VERSÃO ESTÁVEL

--- src/main/java/com/guilherme/cursospring/CursoSpringApplication.java
+++ src/main/java/com/guilherme/cursospring/CursoSpringApplication.java
@@ -1,13 +1,18 @@
 package com.guilherme.cursospring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.guilherme.cursospring.services.S3Service;
+
 
 @SpringBootApplication
 public class CursoSpringApplication implements CommandLineRunner { // Essa interface chama o método run automaticamente
-	
+
+	@Autowired
+	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoSpringApplication.class, args);
@@ -15,9 +20,9 @@ public class CursoSpringApplication implements CommandLineRunner { // Essa inter
 
 	@Override
 	public void run(String... args) throws Exception {
-	
-		
+		s3Service.uploadFile("C:\\Users\\guilh\\OneDrive\\Documentos\\Spring Boot\\aws-logo.png");
+
+
 	}
-
-
+	
 }
