71aa8957ad33944080c5f6cdf87fb1bdcd260f1d
Guilherme Xavier <guilhermexavier2007@hotmail.com>
2022-03-21 08:47:45 -0300

    Revert "Salvando primeiro arquivo no S3"
    This reverts commit 1532cb327e8a1c3fe94bf707a9ce13a95c2045a8.

--- src/main/java/com/guilherme/cursospring/CursoSpringApplication.java
+++ src/main/java/com/guilherme/cursospring/CursoSpringApplication.java
@@ -1,18 +1,13 @@
 package com.guilherme.cursospring;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.guilherme.cursospring.services.S3Service;
 
 
 @SpringBootApplication
 public class CursoSpringApplication implements CommandLineRunner { // Essa interface chama o método run automaticamente
 	
-	@Autowired
-	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoSpringApplication.class, args);
@@ -20,7 +15,6 @@ public class CursoSpringApplication implements CommandLineRunner { // Essa inter
 
 	@Override
 	public void run(String... args) throws Exception {
-		s3Service.uploadFile("C:\\Users\\guilh\\OneDrive\\Documentos\\Spring Boot\\aws-logo.png");
 	
 		
 	}
